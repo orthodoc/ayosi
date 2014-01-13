d38df3f8b0e64689f5b99ff83f46fc8ed419f934
Biswajit Dutta Baruah <bdbaruah@orthodoc.in>
2014-01-13 17:09:49 +0530

    [Finishes #63461638] doc can skip team building

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,6 +2,7 @@ class PatientsController < ApplicationController
   def new
     if signed_in?
       @user = User.find_by_id(current_user.id)
+      @hospital = @user.hospitals.first
       @patient = Patient.new
     else
       flash[:alert] = "You must sign in first!"
